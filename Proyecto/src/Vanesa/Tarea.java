1cb824ddb9216916e7ce71e1df1cdae3f365a16a
vanes <vanes@192.168.1.163>
2022-02-14 21:15:21 -0300

    Presidencia Herencia

--- Proyecto/src/Vanesa/Tarea.java
+++ Proyecto/src/Vanesa/Tarea.java
@@ -1,11 +1,26 @@
 package Vanesa;
-
+import java.io.*;
+import java.util.*;
 public class Tarea {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola mundo");
+		Scanner leer = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero");
+		int n1 = leer.nextInt();
+
+		
+		//System.out.println("Hola mundo");
+		
+		for(int i = 0; i<n1 ;i++) {
+			
+			System.out.println(+ i + "Ingresa un numero");
+			
+			System.out.println(i);
+			
+		}
 
 	}
 
